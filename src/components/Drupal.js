74ec2eec518f267880f557d1f54908745b039bcf
kenorb <kenorb@users.noreply.github.com>
2022-03-07 01:07:54 +0000

    Minor code improvements
    Converts tabs to spaces

--- src/components/Drupal.js
+++ src/components/Drupal.js
@@ -2,23 +2,31 @@ import React from "react";
 import styles from './Drupal.module.css'
 
 export default class Drupal extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	componentDidUpdate(prevProps, prevState) {
-	}
+  // Invoked after a component is mounted (inserted into the tree).
+  async componentDidMount() {
+    const PhpWeb = (await require('php-wasm/PhpWeb')).PhpWeb;
+    this.php = new PhpWeb;
+  }
 
-	async componentDidMount() {
-		const PhpWeb = (await import('php-wasm/PhpWeb')).PhpWeb;
-		const php = new PhpWeb;
-	}
+  // Invoked immediately after updating occurs.
+  componentDidUpdate(prevProps, prevState) {
+  }
 
-	render() {
-		return (
-			<div className={styles.drupal}>
-				Drupal will be here!
-			</div>
-		)
-	}
+  php_test() {
+    //php_test = this.php.run('<?php echo "Hello, world!";');
+    //return php_test.retVal;
+  }
+
+  render() {
+    return (
+      <div className={styles.drupal}>
+        Drupal will be here!
+        {this.php_test()}
+      </div>
+    )
+  }
 }
