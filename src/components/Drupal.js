221920d60bc84095c49ba5fb6c130fbcb4b713e9
kenorb <kenorb@users.noreply.github.com>
2022-06-26 00:01:06 +0100

    Fixes loading issues

--- src/components/Drupal.js
+++ src/components/Drupal.js
@@ -4,12 +4,16 @@ import styles from './Drupal.module.css'
 export default class Drupal extends React.Component {
   constructor(props) {
     super(props);
+    this.php = {};
+    this.state = {ready: false};
   }
 
   // Invoked after a component is mounted (inserted into the tree).
   async componentDidMount() {
     const PhpWeb = (await require('php-wasm/PhpWeb')).PhpWeb;
     this.php = new PhpWeb;
+    console.log(this.php);
+    this.setState({ready: true})
   }
 
   // Invoked immediately after updating occurs.
@@ -17,16 +21,28 @@ export default class Drupal extends React.Component {
   }
 
   php_test() {
-    //php_test = this.php.run('<?php echo "Hello, world!";');
-    //return php_test.retVal;
+    return this.php.run('<?php echo "Hello, world!";').retVal;
   }
 
   render() {
-    return (
-      <div className={styles.drupal}>
-        Drupal will be here!
-        {this.php_test()}
-      </div>
-    )
+    if (this.state.ready) {
+      const hello = this.php_test();
+      console.log(hello);
+      console.log('php_test: ', this.php_test());
+      return (
+          <div className={styles.drupal}>
+            Drupal will be here!
+            {hello}
+            {this.php_test()}
+          </div>
+      )
+    }
+    else {
+      return (
+          <div className={styles.drupal}>
+            Loading...
+          </div>
+      )
+    }
   }
 }
