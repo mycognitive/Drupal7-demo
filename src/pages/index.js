a2f3605d5945d7e0dfa005300bc1270c9bb19b59
kenorb <kenorb@users.noreply.github.com>
2022-07-11 00:04:36 +0100

    Renames Drupal component to separate directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 import dynamic from "next/dynamic";
 import Script from "next/script";
 
-const Drupal = dynamic(() => import("../components/Drupal"), {ssr: false});
+const Drupal = dynamic(() => import("../components/Drupal/Drupal"), {ssr: false});
 
 export default function Home() {
   return (
