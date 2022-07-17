da44634c1652465212f541965afb43dc5e12f44b
kenorb <kenorb@users.noreply.github.com>
2022-07-17 17:06:40 +0100

    Reformats JS code with Prettier

--- src/components/Php/PhpWeb.js
+++ src/components/Php/PhpWeb.js
@@ -1,71 +1,60 @@
-import { PhpBase } from './PhpBase';
+import { PhpBase } from "./PhpBase";
 
-const PhpBinary = require('./php-web');
+const PhpBinary = require("./php-web");
 
-export class PhpWeb extends PhpBase
-{
-	constructor(args = {})
-	{
-		super(PhpBinary, args);
-	}
+export class PhpWeb extends PhpBase {
+  constructor(args = {}) {
+    super(PhpBinary, args);
+  }
 }
 
-if(window && document)
-{
-	const php = new PhpWeb;
+if (window && document) {
+  const php = new PhpWeb();
 
-	const runScriptTag = element => {
+  const runScriptTag = (element) => {
+    const src = element.getAttribute("src");
 
-		const src = element.getAttribute('src');
+    if (src) {
+      fetch(src)
+        .then((r) => r.text())
+        .then((r) => {
+          php.run(r).then((exit) => console.log(exit));
+        });
 
-		if(src)
-		{
-			fetch(src).then(r => r.text()).then(r => {
-				php.run(r).then(exit=>console.log(exit));
-			});
+      return;
+    }
 
-			return;
-		}
+    const inlineCode = element.innerText.trim();
 
-		const inlineCode = element.innerText.trim();
+    if (inlineCode) {
+      php.run(inlineCode);
+    }
+  };
 
-		if(inlineCode)
-		{
-			php.run(inlineCode);
-		}
+  php.addEventListener("ready", () => {
+    const phpSelector = 'script[type="text/php"]';
 
-	};
+    const htmlNode = document.body.parentElement;
+    const observer = new MutationObserver((mutations, observer) => {
+      for (const mutation of mutations) {
+        for (const addedNode of mutation.addedNodes) {
+          if (!addedNode.matches || !addedNode.matches(phpSelector)) {
+            continue;
+          }
 
-	php.addEventListener('ready', () => {
-		const phpSelector = 'script[type="text/php"]';
+          runScriptTag(addedNode);
+        }
+      }
+    });
 
-		const htmlNode = document.body.parentElement;
-		const observer = new MutationObserver((mutations, observer)=>{
-			for(const mutation of mutations)
-			{
-				for(const addedNode of mutation.addedNodes)
-				{
-					if(!addedNode.matches || !addedNode.matches(phpSelector))
-					{
-						continue;
-					}
+    observer.observe(htmlNode, { childList: true, subtree: true });
 
-					runScriptTag(addedNode);
-				}
+    const phpNodes = document.querySelectorAll(phpSelector);
 
-			}
-		});
+    for (const phpNode of phpNodes) {
+      const code = phpNode.innerText.trim();
 
-		observer.observe(htmlNode, {childList: true, subtree: true});
-
-		const phpNodes = document.querySelectorAll(phpSelector);
-
-		for(const phpNode of phpNodes)
-		{
-			const code = phpNode.innerText.trim();
-
-			runScriptTag(phpNode);
-		}
-	});
-
+      runScriptTag(phpNode);
+    }
+  });
 }
