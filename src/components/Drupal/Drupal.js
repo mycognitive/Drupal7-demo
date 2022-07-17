73942510b95d7f26cdc0d17fadd9c907fa6d151d
kenorb <kenorb@users.noreply.github.com>
2022-07-17 16:05:11 +0100

    Moves PHP related methods to Php

--- src/components/Drupal/Drupal.js
+++ src/components/Drupal/Drupal.js
@@ -3,53 +3,7 @@ import React from "react";
 import index from "./index.php"
 import styles from './Drupal.module.css'
 
-export default class Drupal extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.php = {};
-    this.state = {error: '', output: '', pending: false, ready: false};
-  }
-
-  // Invoked after a component is mounted (inserted into the tree).
-  async componentDidMount() {
-    if (!this.state.ready) {
-      try {
-        const PhpWeb = (await require('php-wasm/PhpWeb')).PhpWeb;
-        this.php = new PhpWeb;
-        this.php.addEventListener('error', (event) => {
-          console.error(event.detail[0]);
-          this.setState({error: this.state.error + event.detail[0]});
-        });
-        this.php.addEventListener('output', (event) => {
-          console.log(event);
-          this.setState({output: this.state.output + event.detail[0]});
-          this.setState({pending: false});
-          // console.log(event.detail[0]);
-        });
-        this.php.addEventListener('ready', () => {
-          this.setState({ready: true});
-        });
-      }
-      catch (e) {
-        console.log(e);
-      }
-    }
-  }
-
-  // Invoked immediately after updating occurs.
-  componentDidUpdate(prevProps, prevState) {
-    // console.log('componentDidUpdate');
-  }
-
-  php_hello() {
-    if (!this.state.pending && this.state.output == '') {
-      this.php.run('<?php echo "Hello, world!";')
-        .then(retVal => {
-          this.setState({pending: true});
-      });
-    }
-  }
+export default class Drupal extends Php {
 
   php_index() {
     if (!this.state.pending && this.state.output == '') {
@@ -61,31 +15,11 @@ export default class Drupal extends React.Component {
     }
   }
 
-  php_info() {
-    if (!this.state.pending && this.state.output == '') {
-      this.php.run('<?php php_info();')
-        .then(retVal => {
-          this.setState({pending: true});
-      });
-    }
-  }
-
   render() {
-    if (this.state.ready) {
-      this.php_index();
-      return (
-        <iframe
-          className={styles.drupal}
-          sandbox="allow-same-origin allow-scripts allow-forms"
-          srcDoc={this.state.output} />
-      )
-    }
-    else {
-      return (
-        <div className={styles.drupal}>
-          Loading...
-        </div>
-      )
-    }
+    return (
+      <div className={styles.drupal}>
+        <Php/>
+      </div>
+    )
   }
 }
--- src/components/Php/Php.js
+++ src/components/Php/Php.js
@@ -1,5 +1,7 @@
-require("./php-web.js")
 import React from "react";
+import styles from './Php.module.css'
+
+//require("./php-web.js")
 
 export default class Php extends React.Component {
 
@@ -9,17 +11,68 @@ export default class Php extends React.Component {
     this.state = {error: '', output: '', pending: false, ready: false};
   }
 
+  // Invoked after a component is mounted (inserted into the tree).
+  async componentDidMount() {
+    if (!this.state.ready) {
+      try {
+        const PhpWeb = (await require('php-wasm/PhpWeb')).PhpWeb;
+        this.php = new PhpWeb;
+        this.php.addEventListener('error', (event) => {
+          console.error(event.detail[0]);
+          this.setState({error: this.state.error + event.detail[0]});
+        });
+        this.php.addEventListener('output', (event) => {
+          console.log(event);
+          this.setState({output: this.state.output + event.detail[0]});
+          this.setState({pending: false});
+          // console.log(event.detail[0]);
+        });
+        this.php.addEventListener('ready', () => {
+          this.setState({ready: true});
+        });
+      }
+      catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  // Invoked immediately after updating occurs.
+  componentDidUpdate(prevProps, prevState) {
+    // console.log('componentDidUpdate');
+  }
+
+  php_hello() {
+    if (!this.state.pending && this.state.output == '') {
+      this.php.run('<?php echo "Hello, world!";')
+        .then(retVal => {
+          this.setState({pending: true});
+      });
+    }
+  }
+
+  php_info() {
+    if (!this.state.pending && this.state.output == '') {
+      this.php.run('<?php phpinfo();')
+        .then(retVal => {
+          this.setState({pending: true});
+      });
+    }
+  }
+
   render() {
     if (this.state.ready) {
+      this.php_info();
       return (
-        <div>
-          Hello World!
-        </div>
+        <iframe
+          className={styles.php}
+          sandbox="allow-same-origin allow-scripts allow-forms"
+          srcDoc={this.state.output} />
       )
     }
     else {
       return (
-        <div>
+        <div className={styles.php}>
           Loading...
         </div>
       )
