2c7fb9ea8f0345ee24c174afb8ec621069335d5a
kenorb <kenorb@users.noreply.github.com>
2022-07-24 15:09:13 +0100

    Adds code to load preload files
    Based on some code from seanmorris/php-wasm repo
    
    Co-Authored-By: Sean Morris <640101+seanmorris@users.noreply.github.com>

--- src/components/Php/php-web.js
+++ src/components/Php/php-web.js
@@ -13,6 +13,222 @@ var PHP = (function () {
       readyPromiseResolve = resolve;
       readyPromiseReject = reject;
     });
+    if (!Module.expectedDataFileDownloads) {
+      Module.expectedDataFileDownloads = 0;
+    }
+    Module.expectedDataFileDownloads++;
+    (function () {
+      var loadPackage = function (metadata) {
+        var PACKAGE_PATH;
+        if (typeof window === "object") {
+          PACKAGE_PATH = window["encodeURIComponent"](
+            window.location.pathname
+              .toString()
+              .substring(
+                0,
+                window.location.pathname.toString().lastIndexOf("/")
+              ) + "/"
+          );
+        } else if (typeof location !== "undefined") {
+          PACKAGE_PATH = encodeURIComponent(
+            location.pathname
+              .toString()
+              .substring(0, location.pathname.toString().lastIndexOf("/")) + "/"
+          );
+        } else {
+          throw "using preloaded data can only be done on a web page or in a web worker";
+        }
+        var PACKAGE_NAME = "../../build/drupal-8.9.20.zip";
+        var REMOTE_PACKAGE_BASE = "_next/static/chunks/drupal-8.9.20.zip";
+        if (
+          typeof Module["locateFilePackage"] === "function" &&
+          !Module["locateFile"]
+        ) {
+          Module["locateFile"] = Module["locateFilePackage"];
+          err(
+            "warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)"
+          );
+        }
+        var REMOTE_PACKAGE_NAME = Module["locateFile"]
+          ? Module["locateFile"](REMOTE_PACKAGE_BASE, "")
+          : REMOTE_PACKAGE_BASE;
+        var REMOTE_PACKAGE_SIZE = metadata["remote_package_size"];
+        var PACKAGE_UUID = metadata["package_uuid"];
+        function fetchRemotePackage(
+          packageName,
+          packageSize,
+          callback,
+          errback
+        ) {
+          var xhr = new XMLHttpRequest();
+          xhr.open("GET", packageName, true);
+          xhr.responseType = "arraybuffer";
+          xhr.onprogress = function (event) {
+            var url = packageName;
+            var size = packageSize;
+            if (event.total) size = event.total;
+            if (event.loaded) {
+              if (!xhr.addedTotal) {
+                xhr.addedTotal = true;
+                if (!Module.dataFileDownloads) Module.dataFileDownloads = {};
+                Module.dataFileDownloads[url] = {
+                  loaded: event.loaded,
+                  total: size,
+                };
+              } else {
+                Module.dataFileDownloads[url].loaded = event.loaded;
+              }
+              var total = 0;
+              var loaded = 0;
+              var num = 0;
+              for (var download in Module.dataFileDownloads) {
+                var data = Module.dataFileDownloads[download];
+                total += data.total;
+                loaded += data.loaded;
+                num++;
+              }
+              total = Math.ceil(
+                (total * Module.expectedDataFileDownloads) / num
+              );
+              if (Module["setStatus"])
+                Module["setStatus"](
+                  "Downloading data... (" + loaded + "/" + total + ")"
+                );
+            } else if (!Module.dataFileDownloads) {
+              if (Module["setStatus"])
+                Module["setStatus"]("Downloading data...");
+            }
+          };
+          xhr.onerror = function (event) {
+            throw new Error("NetworkError for: " + packageName);
+          };
+          xhr.onload = function (event) {
+            if (
+              xhr.status == 200 ||
+              xhr.status == 304 ||
+              xhr.status == 206 ||
+              (xhr.status == 0 && xhr.response)
+            ) {
+              var packageData = xhr.response;
+              callback(packageData);
+            } else {
+              throw new Error(xhr.statusText + " : " + xhr.responseURL);
+            }
+          };
+          xhr.send(null);
+        }
+        function handleError(error) {
+          console.error("package error:", error);
+        }
+        var fetchedCallback = null;
+        var fetched = Module["getPreloadedPackage"]
+          ? Module["getPreloadedPackage"](
+              REMOTE_PACKAGE_NAME,
+              REMOTE_PACKAGE_SIZE
+            )
+          : null;
+        if (!fetched)
+          fetchRemotePackage(
+            REMOTE_PACKAGE_NAME,
+            REMOTE_PACKAGE_SIZE,
+            function (data) {
+              if (fetchedCallback) {
+                fetchedCallback(data);
+                fetchedCallback = null;
+              } else {
+                fetched = data;
+              }
+            },
+            handleError
+          );
+        function runWithFS() {
+          function assert(check, msg) {
+            if (!check) throw msg + new Error().stack;
+          }
+          Module["FS_createPath"]("/", "preload", true, true);
+          Module["FS_createPath"]("/preload", "drupal", true, true);
+          Module["FS_createPath"]("/preload", "Zend", true, true);
+          function DataRequest(start, end, audio) {
+            this.start = start;
+            this.end = end;
+            this.audio = audio;
+          }
+          DataRequest.prototype = {
+            requests: {},
+            open: function (mode, name) {
+              this.name = name;
+              this.requests[name] = this;
+              Module["addRunDependency"]("fp " + this.name);
+            },
+            send: function () {},
+            onload: function () {
+              var byteArray = this.byteArray.subarray(this.start, this.end);
+              this.finish(byteArray);
+            },
+            finish: function (byteArray) {
+              var that = this;
+              Module["FS_createDataFile"](
+                this.name,
+                null,
+                byteArray,
+                true,
+                true,
+                true
+              );
+              Module["removeRunDependency"]("fp " + that.name);
+              this.requests[this.name] = null;
+            },
+          };
+          var files = metadata["files"];
+          for (var i = 0; i < files.length; ++i) {
+            new DataRequest(
+              files[i]["start"],
+              files[i]["end"],
+              files[i]["audio"]
+            ).open("GET", files[i]["filename"]);
+          }
+          function processPackageData(arrayBuffer) {
+            assert(arrayBuffer, "Loading data file failed.");
+            assert(
+              arrayBuffer instanceof ArrayBuffer,
+              "bad input to processPackageData"
+            );
+            var byteArray = new Uint8Array(arrayBuffer);
+            DataRequest.prototype.byteArray = byteArray;
+            var files = metadata["files"];
+            for (var i = 0; i < files.length; ++i) {
+              DataRequest.prototype.requests[files[i].filename].onload();
+            }
+            Module["removeRunDependency"](
+              "datafile_drupal-8.9.20.zip"
+            );
+          }
+          Module["addRunDependency"](
+            "datafile_drupal-8.9.20.zip"
+          );
+          if (!Module.preloadResults) Module.preloadResults = {};
+          Module.preloadResults[PACKAGE_NAME] = { fromCache: false };
+          if (fetched) {
+            processPackageData(fetched);
+            fetched = null;
+          } else {
+            fetchedCallback = processPackageData;
+          }
+        }
+        if (Module["calledRun"]) {
+          runWithFS();
+        } else {
+          if (!Module["preRun"]) Module["preRun"] = [];
+          Module["preRun"].push(runWithFS);
+        }
+      };
+      loadPackage({
+        files: [
+        ],
+        remote_package_size: 0,
+        package_uuid: "810f12dc-218a-40e5-b332-094cc42aac8b",
+      });
+    })();
     var moduleOverrides = {};
     var key;
     for (key in Module) {
@@ -7284,6 +7500,14 @@ var PHP = (function () {
     });
     FS.FSNode = FSNode;
     FS.staticInit();
+    Module["FS_createFolder"] = FS.createFolder;
+    Module["FS_createPath"] = FS.createPath;
+    Module["FS_createDataFile"] = FS.createDataFile;
+    Module["FS_createPreloadedFile"] = FS.createPreloadedFile;
+    Module["FS_createLazyFile"] = FS.createLazyFile;
+    Module["FS_createLink"] = FS.createLink;
+    Module["FS_createDevice"] = FS.createDevice;
+    Module["FS_unlink"] = FS.unlink;
     function intArrayFromString(stringy, dontAddNull, length) {
       var len = length > 0 ? length : lengthBytesUTF8(stringy) + 1;
       var u8array = new Array(len);
@@ -7776,8 +8000,19 @@ var PHP = (function () {
         Module["asm"]["qc"]).apply(null, arguments);
     });
     Module["ccall"] = ccall;
+    Module["getMemory"] = getMemory;
     Module["UTF8ToString"] = UTF8ToString;
     Module["lengthBytesUTF8"] = lengthBytesUTF8;
+    Module["addRunDependency"] = addRunDependency;
+    Module["removeRunDependency"] = removeRunDependency;
+    Module["FS_createFolder"] = FS.createFolder;
+    Module["FS_createPath"] = FS.createPath;
+    Module["FS_createDataFile"] = FS.createDataFile;
+    Module["FS_createPreloadedFile"] = FS.createPreloadedFile;
+    Module["FS_createLazyFile"] = FS.createLazyFile;
+    Module["FS_createLink"] = FS.createLink;
+    Module["FS_createDevice"] = FS.createDevice;
+    Module["FS_unlink"] = FS.unlink;
     var calledRun;
     function ExitStatus(status) {
       this.name = "ExitStatus";
