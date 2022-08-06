45d78959ace19f48b3bbf34aa3505ecb75990cec
kenorb <kenorb@users.noreply.github.com>
2022-08-06 22:27:11 +0100

    Reformats Next config file

--- next.config.js
+++ next.config.js
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
-const CopyPlugin = require('copy-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
       {
-        source: '/admin/(.*)',
-        destination: '/drupal',
-      }
-    ]
+        source: "/admin/(.*)",
+        destination: "/drupal",
+      },
+    ];
   },
   // Perform customizations to webpack config.
   webpack: function (config, options) {
     console.log("Webpack version:", options.webpack.version); // 5.18.0
     config.experiments = {
       ...config.experiments,
-      asyncWebAssembly: true
+      asyncWebAssembly: true,
     };
     config.module.rules = [
       ...config.module.rules,
-      { test: /\.php$/, use: 'raw-loader' }
+      { test: /\.php$/, use: "raw-loader" },
     ];
     config.plugins = [
       ...config.plugins,
@@ -27,21 +27,21 @@ const nextConfig = {
         patterns: [
           {
             from: "./build/drupal-8.9.20.zip",
-            to: "./static/build/"
+            to: "./static/build/",
           },
           {
             from: "./node_modules/php-wasm/php-web.wasm",
-            to: "./static/chunks/"
+            to: "./static/chunks/",
           },
           {
             from: "./node_modules/php-wasm/php-web-drupal.wasm",
-            to: "./static/chunks/"
-          }
+            to: "./static/chunks/",
+          },
         ],
       }),
     ];
     return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
