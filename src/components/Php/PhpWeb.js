ee0b4a610f513c1cee38f1846a169db6afb9729d
kenorb <kenorb@users.noreply.github.com>
2022-08-14 01:54:56 +0100

    PhpWeb: Adds args overrides

--- src/components/Php/PhpWeb.js
+++ src/components/Php/PhpWeb.js
@@ -9,7 +9,40 @@ export class PhpWeb extends PhpBase {
 }
 
 if (window && document) {
-  const php = new PhpWeb();
+  var args = {
+    INITIAL_MEMORY: 1073741824,
+    arguments: [],
+    expectedDataFileDownloads: 1,
+    getPreloadedPackage: function (pkg_name, pkg_size) {
+      return true;
+    },
+    noExitRuntime: false,
+    onAbort: function (what) {
+      console.error(what);
+    },
+    onRuntimeInitialized: function () {
+      console.log("onRuntimeInitialized");
+    },
+    onFileMissing: function (path, node) {
+      console.log(path, node);
+    },
+    preInit: [], // List of functions to call.
+    preRun: [], // List of functions to call.
+    preloadedImages: {},
+    print: console.log.bind(console),
+    printErr: console.warn.bind(console),
+    quit: function (status, toThrow) {
+      console.error(status);
+      throw toThrow;
+    },
+    setStatus: function (status) {
+      console.log(status);
+    },
+    thisProgram: "NextJS",
+    // "locateFile": function(file, size) { return "WASM"; },
+    // "wasmBinary": "foo",
+  };
+  const php = new PhpWeb(args);
 
   const runScriptTag = (element) => {
     const src = element.getAttribute("src");
